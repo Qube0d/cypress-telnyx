7ab7c0f39df0326b81fd646adce22b38c40619ba
Qube0d <smetacometa@gmail.com>
2022-10-14 21:18:41 +0300

    allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,39 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+//const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 
 
 
 module.exports = defineConfig({
-           chromeWebSecurity: false ,
   e2e: {
     setupNodeEvents(on, config) {
-       chromeWebSecurity: false 
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
-  },
-});
+  chromeWebSecurity: false 
+  }
+})
+
+
+
+
+
+// module.exports = defineConfig({
+//   e2e: {
+//       async setupNodeEvents(on, config) {
+//       const bundler = createBundler({
+//         plugins: [createEsbuildPlugin(config)],
+//       });
+
+//       on("file:preprocessor", bundler);
+//       allureWriter(on, config);
+//       await addCucumberPreprocessorPlugin(on, config);
+//       return config;
+//     },
+//     //   setupNodeEvents(on, config) {
+//     //    chromeWebSecurity: false
+//     //   // implement node event listeners here
+//     // },
+//   chromeWebSecurity: false ,
+//   },
+// });
